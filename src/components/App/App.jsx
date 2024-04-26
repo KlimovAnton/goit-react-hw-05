fd2434f1dc5a6c0f8b5076a70ad9d321680c6158
Anton <antonklim6@gmail.com>
2024-04-26 22:06:27 +0200

    add api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import { getMovies } from '../../movies-api';
 // import SearchMovies from "../SearchMovies/SearchMovies";
-import MoviesList from "../MoviesList/MoviesList";
-
+// import MoviesList from "../MoviesList/MoviesList";
+console.log(getMovies())
 export default function App () {
   return (
     <>
@@ -12,7 +13,7 @@ export default function App () {
           <Route path="/movies" />
         </Routes>
       </Layout>
-      <MoviesList />
+      {/* <MoviesList /> */}
     </>
   )
 }
--- src/movies-api.js
+++ src/movies-api.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+const KEY = "36271bde09fb3588e38e08fb70b0c966";
 
 export const getMovies = async () => {
-    const response = await axios.get("trending/movie/day?language=en-US");
-     response.data;
+    const response = await axios.get("trending/movie/day?language=en-US", {
+      headers: {
+        Authorization: 
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjI3MWJkZTA5ZmIzNTg4ZTM4ZTA4ZmI3MGIwYzk2NiIsInN1YiI6IjY2MmJmZWU5NGEyMjI2MDEyODU4Yzk1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VlMFzVsMvcSowwOTGN8Iy4d8mKEATM_ZSYPB0wLEqEs",
+      },
+    });
+     return response.data.results;
   };
   
 //   export const getMovieById = async (paymentId) => {
 //     const response = await axios.get(`/payments/${paymentId}`);
 //     return response.data;
 //   };
+
+
